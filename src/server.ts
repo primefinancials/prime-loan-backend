627e789794131875a096171c31e7496845fca087
ZE-BOSS <ikchristo19@gmail.com>
2025-02-27 16:12:03 +0100

    latest

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from "express";
 import http from "http";
 import { connectToDB } from "./utils";
 import { PORT } from "./config";
-import { checkLoansAndSendEmails } from "./jobs/loanReminder";
+import { checkLoansAndSendEmails, addOnePercentToOverdueLoan } from "./jobs/loanReminder";
 import cron from 'node-cron';
 import { sendEmail } from "./jobs/loanReminder";
 
@@ -12,10 +12,10 @@ cron.schedule('0 */6 * * *', async () => {
   await checkLoansAndSendEmails();
 });
 
-// cron.schedule('*/1 * * * *', async () => {
-//   console.log('Running loan check every minute...');
-//   await sendEmail("primefinancials68@gmail.com", 'Loan Overdue', `Test Email For Loan`);
-// });
+cron.schedule('0 0 * * *', async () => {
+  console.log('Running loan check once a day...');
+  await addOnePercentToOverdueLoan();
+});
 
 console.log('Cron job scheduled to check loans daily at midnight.');
 
