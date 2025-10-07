859f8b765e4e2a279e5a61adb99f522e5f80faae
Savage57 <your_email@example.com>
2025-10-08 00:02:47 +0100

    refactor: reorganize and format CORS options for improved readability

--- src/shared/utils/cross-origin.ts
+++ src/shared/utils/cross-origin.ts
@@ -1,59 +1,64 @@
 import cors, { CorsOptions } from 'cors';
 
-
 type CrossOrigin = Partial<typeof DefaultCorsOptions>;
-
 
 /**
  * Default configuration options for cross-origin requests.
  */
 const DefaultCorsOptions: CorsOptions = {
-    /**
-     * The allowed origin(s) for the request. Use '*' to allow all origins.
-     */
-    origin: [
-        '*', 
-        'http://localhost:3000',
-        'http://localhost:3001',
-        'http://localhost:5173', 
-        'http://localhost:5174', 
-        'http://localhost:8081', 
-        'https://prime-finance-admin.netlify.app', 
-        'https://admin.primefinance.live',
-        'https://primefinance.live',
-        'https://prime-loan-web-init.vercel.app'
-    ],
-    /**
-     * The allowed HTTP methods for the request.
-     */
-    methods: 'PUT, GET, PATCH, DELETE, POST, OPTIONS',
+  /**
+   * The allowed origin(s) for the request. Use '*' to allow all origins.
+   */
+  origin: [
+    '*',
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://localhost:8081',
+    'https://prime-finance-admin.netlify.app',
+    'https://admin.primefinance.live',
+    'https://primefinance.live',
+    'https://prime-loan-web-init.vercel.app',
+  ],
 
-    /**
-     * Whether to continue with the preflight request if the initial request is successful.
-     */
-    preflightContinue: false,
-    /**
-     * The HTTP status code to use for successful OPTIONS requests.
-     */
-    optionsSuccessStatus: 200,
-    /**
-     * Whether to include credentials (such as cookies or authorization headers) with the request.
-     */
-    credentials: true,
-    /**
-     * The maximum age (in seconds) to cache the preflight response.
-     */
-    maxAge: 30 * 60 * 60 * 24 * 1000, // 30 days 
-    /**
-     * The headers that are exposed to the browser in the response.
-     */
-    exposedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, x-csrf-token, Authorization, X-App-Platform',
-    /**
-     * The allowed headers for the request.
-     */
-    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, x-csrf-token, Authorization, X-App-Platform',
+  /**
+   * The allowed HTTP methods for the request.
+   */
+  methods: 'PUT, GET, PATCH, DELETE, POST, OPTIONS',
+
+  /**
+   * Whether to continue with the preflight request if the initial request is successful.
+   */
+  preflightContinue: false,
+
+  /**
+   * The HTTP status code to use for successful OPTIONS requests.
+   */
+  optionsSuccessStatus: 200,
+
+  /**
+   * Whether to include credentials (such as cookies or authorization headers) with the request.
+   */
+  credentials: true,
+
+  /**
+   * The maximum age (in seconds) to cache the preflight response.
+   */
+  maxAge: 30 * 60 * 60 * 24 * 1000, // 30 days
+
+  /**
+   * The headers that are exposed to the browser in the response.
+   */
+  exposedHeaders:
+    'Origin, X-Requested-With, Content-Type, Accept, x-csrf-token, Authorization, X-App-Platform, Idempotency-Key',
+
+  /**
+   * The allowed headers for the request.
+   */
+  allowedHeaders:
+    'Origin, X-Requested-With, Content-Type, Accept, x-csrf-token, Authorization, X-App-Platform, Idempotency-Key',
 };
-
 
 const crossOrigin = (options: CrossOrigin = DefaultCorsOptions) => cors(options);
 
