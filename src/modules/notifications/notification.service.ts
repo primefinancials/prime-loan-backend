07dad91ded52f2e8e06a23b36d880f17476a237f
Savage57 <your_email@example.com>
2025-10-15 01:35:20 +0100

    Fix email sender format to include sender name in notification service

--- src/modules/notifications/notification.service.ts
+++ src/modules/notifications/notification.service.ts
@@ -14,7 +14,7 @@ const transporter = nodemailer.createTransport({
 export class NotificationService {
   private static async sendEmail(to: string, subject: string, html: string) {
     await transporter.sendMail({
-      from: `${process.env.EMAIL_USERNAME}`,
+      from: `Prime Finance <${process.env.EMAIL_USERNAME}>`,
       to,
       subject,
       html,
